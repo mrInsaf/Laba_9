54a970bc5be954193645869648579fac8cd7e7f8
mrInsaf <mr.insaf@list.ru>
2023-06-15 22:47:34 +0300

    added interface

---
+++ classes_and_functions.py
@@ -0,0 +1,77 @@
+class Figure:
+    letters = "_ABCDEFGH"
+
+    def __init__(self, x, y):
+        self.y = y
+        self.x = self.letters.find(x)
+
+    def __set_x(self, x):
+        self.x = self.letters.find(x)
+
+    def __set_y(self, y):
+        self.y = y
+
+    def get_coordinates(self):
+        return self.x, self.y
+
+    def move(self, dest_x, dest_y):
+        self.__set_x(dest_x)
+        self.__set_y(dest_y)
+
+    def raise_error(self):
+        print(f'Введите корректные данные для класса {self.__class__.__name__}')
+
+
+class Bishop(Figure):
+    def move(self, dest_x, dest_y):
+        if abs(self.x - dest_x) == abs(self.y - dest_y):
+            super().move(dest_x, dest_y)
+        else:
+            self.raise_error()
+
+
+def print_chessboard(figures):
+    size = 8
+    letters = "ABCDEFGH"  # Список букв для отображения столбцов
+    for row in range(size, 0, -1):  # Изменяем порядок вывода строк снизу вверх
+        print(row, end="  ")  # Выводим цифру строки
+        for col in range(1, 9):
+            for figure in figures:
+                coor = figure.get_coordinates()
+                if row == coor[1] and col == coor[0]:
+                    if figure.__class__.__name__ == 'Bishop':
+                        print('♗', end="  ")
+                    else:
+                        print('*', end="  ")
+                    break
+            else:
+                if (row + col) % 2 == 0:
+                    print("■", end="  ")
+                else:
+                    print("□", end="  ")
+        print()
+    print("   ", end="")  # Выводим пробелы перед буквами столбцов
+    for col in range(size):
+        print(letters[col], end="  ")  # Выводим букву столбца
+    print()
+
+
+def input_coors():
+    while True:
+        coors = input('Введите координаты фигуры, например, A2 или F8\n')
+
+        if len(coors) != 2:
+            print('\nКоличество координат должно быть 2!\n')
+            continue
+
+        x = coors[0]
+        y = int(coors[1])
+
+        if x not in Figure.letters:
+            print('Первая координата должна быть A-H')
+            continue  # Возвращаемся к вводу координат
+        elif y not in range(1, 9):
+            print("Вторая координата должна быть 1-8")
+            continue  # Возвращаемся к вводу координат
+        else:
+            return x, y
--- main.py
+++ main.py
@@ -1,79 +1,29 @@
-class Figure:
-
-    letters = "ABCDEFGH"
-    def __init__(self, x, y):
-        self.y = y
-        self.x = self.letters.find(x)
-
-    def __set_x(self, x):
-        self.x = self.letters.find(x)
-
-    def __set_y(self, y):
-        self.y = y
-
-    def get_coordinates(self):
-        return self.x, self.y
-
-    def move(self, dest_x, dest_y):
-        self.__set_x(dest_x)
-        self.__set_y(dest_y)
-
-    def raise_error(self):
-        print(f'Введите корректные данные для класса {self.__class__.__name__}')
-
-
-class Bishop(Figure):
-    def move(self, dest_x, dest_y):
-        if abs(self.x - dest_x) == abs(self.y - dest_y):
-            super().move(dest_x, dest_y)
-        else:
-            self.raise_error()
-
-
-def print_chessboard(figures):
-    size = 8
-    letters = "ABCDEFGH"  # Список букв для отображения столбцов
-    for row in range(size - 1, -1, -1):  # Изменяем порядок вывода строк снизу вверх
-        print(row + 1, end="  ")  # Выводим цифру строки
-        for col in range(size):
-            for figure in figures:
-                coor = figure.get_coordinates()
-                if row == coor[0] and col == coor[1]:
-                    if figure.__class__.__name__ == 'Bishop':
-                        print('♗', end="  ")
-                    else:
-                        print('*', end="  ")
-                    break
-                else:
-                    if (row + col) % 2 == 0:
-                        print("■", end="  ")
-                    else:
-                        print("□", end="  ")
-        print()
-    print("   ", end="")  # Выводим пробелы перед буквами столбцов
-    for col in range(size):
-        print(letters[col], end="  ")  # Выводим букву столбца
-    print()
-
-
-
+from classes_and_functions import *
 
 figures = []
 
+while True:
+    inp = int(input(
+        'Выберите желаемое действие\n [1] - Вывести текущую доску\n [2] - Добавить фигуру\n [3] - Переместить фигуру\n'))
+    if inp == 1:
+        print('Текущая доска: \n')
+        print_chessboard(figures)
+        print()
+    if inp == 2:
+        inp_2 = int(input('Добавить\n [1] - Слона, [2] - Просто фигуру\n'))
+        x, y = input_coors()
 
 
+        if inp_2 == 1:
+            new_bishop = Bishop(x, y)
+            figures.append(new_bishop)
+        elif inp_2 == 2:
+            new_figure = Figure(x, y)
+            figures.append(new_figure)
 
-my_bishop = Bishop('A', 0)
-print(my_bishop.get_coordinates())
-# my_figure = Figure(4, 5)
-
-
-
-
-figures.append(my_bishop)
-# figures[0].move(4, 7)
-# print(figures[0].get_coordinates())
-# # print(len(figures))
-#
-print_chessboard(figures)  # Выводит подобие шахматной доски 8x8
+    if inp == 3:
+        print('Введите координаты фигуры, которую хотите передвинуть\n')
+        start_x, start_y = input_coors()
+        print('\nКуда передвинуть?\n')
+        dest_x, dest_y = input_coors()
 
