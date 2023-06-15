3a3c162c875d88d83c8b87012db4e00413e83b2b
mrInsaf <mr.insaf@list.ru>
2023-06-15 20:31:13 +0300

    finished Bishop class added raise_error method to Figure

--- main.py
+++ main.py
@@ -9,11 +9,31 @@ class Figure:
     def set_y(self, y):
         self.y = y
 
+    def get_coordinates(self):
+        print(self.x, self.y)
+        return self.x, self.y
+
+    def move(self, dest_x, dest_y):
+        self.set_x(dest_x)
+        self.set_y(dest_y)
+
+    def raise_error(self):
+        print(f'Введите корректные данные для класса {self.__class__.__name__}')
+
 
 class Bishop(Figure):
-    def move(self, x, y):
-        self.set_x(x)
-        self.set_y(y)
+    def move(self, dest_x, dest_y):
+        if abs(self.x - dest_x) == abs(self.y - dest_y):
+            super().move(dest_x, dest_y)
+        else:
+            self.raise_error()
 
 
-        
+my_bishop = Bishop(6, 6)
+my_bishop.get_coordinates()
+my_bishop.move(6, 7)
+my_bishop.get_coordinates()
+
+
+
+
