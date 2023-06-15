13b0fb7cb6bd460825f4fbe0f23ed9b586b85442
mrInsaf <mr.insaf@list.ru>
2023-06-15 21:21:23 +0300

    added figures to the board

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 class Figure:
+
+    letters = "ABCDEFGH"
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def set_x(self, x):
+    def __set_x(self, x):
         self.x = x
 
-    def set_y(self, y):
+    def __set_y(self, y):
         self.y = y
 
     def get_coordinates(self):
-        print(self.x, self.y)
         return self.x, self.y
 
     def move(self, dest_x, dest_y):
-        self.set_x(dest_x)
-        self.set_y(dest_y)
+        self.__set_x(dest_x)
+        self.__set_y(dest_y)
 
     def raise_error(self):
         print(f'Введите корректные данные для класса {self.__class__.__name__}')
@@ -29,25 +30,46 @@ class Bishop(Figure):
             self.raise_error()
 
 
-def print_chessboard(size):
-    for row in range(size):
+def print_chessboard(figures):
+    size = 8
+    letters = "ABCDEFGH"  # Список букв для отображения столбцов
+    for row in range(size - 1, -1, -1):  # Изменяем порядок вывода строк снизу вверх
+        print(row + 1, end="  ")  # Выводим цифру строки
         for col in range(size):
-            if (row + col) % 2 == 0:
-                print("■", end="  ")  # символ для черных клеток
-            else:
-                print("□", end="  ")  # символ для белых клеток
+            for figure in figures:
+                coor = figure.get_coordinates()
+                if row == coor[0] and col == coor[1]:
+                    if figure.__class__.__name__ == 'Bishop':
+                        print('B', end="  ")
+                    else:
+                        print('*', end="  ")
+                    break
+                else:
+                    if (row + col) % 2 == 0:
+                        print("■", end="  ")
+                    else:
+                        print("□", end="  ")
         print()
+    print("   ", end="")  # Выводим пробелы перед буквами столбцов
+    for col in range(size):
+        print(letters[col], end="  ")  # Выводим букву столбца
+    print()
 
-# Пример использования функции
-print_chessboard(8)  # Выводит подобие шахматной доски 8x8
+
+
+
+figures = []
+
 
 
 
 my_bishop = Bishop(6, 6)
-my_bishop.get_coordinates()
-my_bishop.move(6, 7)
-my_bishop.get_coordinates()
 
 
+figures.append(my_bishop)
+figures[0].move(4, 7)
+print(figures[0].get_coordinates())
+# print(len(figures))
 
+print_chessboard(figures)  # Выводит подобие шахматной доски 8x8
 
